4251182fae9a5f6cee678f3f0a3778aca27da92f
mau <mauricio.medrano051@gmail.com>
2022-05-28 18:05:15 -0500

    feat: add functinality to buttons

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,29 @@
 function App() {
-  console.log("This is the app");
+  const ruidoElementArray = Array.from(
+    document.querySelectorAll(`div[data-ruido="true"]`)
+  );
+
+  function desactivateButton(ruidoButton) {
+    console.log(ruidoButton);
+    ruidoButton.removeAttribute('disabled');
+  }
+
+  ruidoElementArray.forEach((element) => {
+    const ruidoButton = element.firstElementChild;
+    const audio = element.lastElementChild;
+
+    audio.addEventListener('load', desactivateButton(ruidoButton));
+
+    function stopSong() {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+
+    ruidoButton.addEventListener('click', () => {
+      stopSong();
+      audio.play();
+    });
+  });
 }
 
 App();
