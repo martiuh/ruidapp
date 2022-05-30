0ad6d12c48019f90776d8ca13eb2ec5de028adcf
Tona González <martiuh@gmail.com>
2022-05-29 19:10:28 -0500

    feat: deply

--- moveSrc.js
+++ moveSrc.js
@@ -1,7 +1,13 @@
-const fs = require('fs');
+const fse = require('fs-extra');
 
-module.exports = (endCallback) => {
-  fs.cp(`${__dirname}/src`, `${__dirname}/.vercel/output/static`, () =>
-    endCallback()
-  );
-};
+const srcDir = `${__dirname}/src`;
+const destDir = `${__dirname}/public`;
+
+// To copy a folder or file
+fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('success!');
+  }
+});
