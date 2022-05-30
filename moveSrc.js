34071305abc2ad56399bbabb92fd74d2ee3cbf5d
Tona González <martiuh@gmail.com>
2022-05-29 19:28:13 -0500

    fix: ensure existance of public dir

--- moveSrc.js
+++ moveSrc.js
@@ -3,11 +3,15 @@ const fse = require('fs-extra');
 const srcDir = `${__dirname}/src`;
 const destDir = `${__dirname}/public`;
 
-// To copy a folder or file
-fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('success!');
-  }
-});
+fse.rmdir(destDir, () =>
+  fse.mkdir(destDir, () => {
+    // To copy a folder or file
+    fse.copySync(srcDir, destDir, { overwrite: true }, function (err) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('success!');
+      }
+    });
+  })
+);
